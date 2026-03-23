0caf0ad3ffa6f843c9b2dc849d11a4cf3f7d3d31
loliette <yelmaniyel@gmail.com>
2026-03-23 15:01:29 +0000

    opengraph balise forms en cours

--- statapp/context_processors.py
+++ statapp/context_processors.py
@@ -9,8 +9,9 @@ def categories_processor(request):
 def contact_processor(request):
     """Rend les informations de contact disponibles partout dans le projet"""
     return {
-        'contact_email': 'stat@gmail.com',
+        'contact_email': 'votre_email@gmail.com',
         'contact_res': '+226 72 78 72 15',
+        'whatsapp_number':'+22672787215',
         'contact_res2': '+226 74 47 81 12',
-        'address': 'Bobo-Dioulasso, Burkina Faso'
+        'address': 'Bobo-Dioulasso,Burkina Faso'
     }
